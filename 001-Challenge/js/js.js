423954226906b2dcfdc7dd3e310cc551b91a7d3c
luizrcruzo <luiz.cruz@alfacruz.com.br>
2021-04-04 00:14:52 -0300

    First Javascript challenge

---
+++ 001-Challenge/js/js.js
@@ -0,0 +1,13 @@
+//variables definitions
+var mission = "001 - Challenge JS!";
+
+//functions
+function eventoOnLoad() {
+    console.log("Onload event launched!");    
+}
+
+function eventoOnClick() {
+    console.log("Challenge succesfully accomplished!"); 
+    alert("Challenge succesfully acomplished!");    
+}
+
